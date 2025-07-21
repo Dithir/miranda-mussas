001a4fa76c0ff89a904b76e55b26e997ca5aa05e
Dithir <agustinbaez94@gmail.com>
2025-07-21 20:44:20 -0300

    LaunchModal
    Added a Logo modal on page open

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,7 @@ import {
   ScrollRestoration,
   Link
 } from "react-router";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import type { Route } from "./+types/root";
 import "./app.css";
@@ -47,8 +47,37 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export default function App() {
 
+  const [launchModal, setLaunchModal] = useState(true);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLaunchModal(false);
+    }, 2800);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
     <>
+    {launchModal && (
+            <div
+                className="fixed inset-0 bg-black/70 bg-opacity-50 flex items-center justify-center z-50"
+            > 
+              <div className="animate-shrink-grow w-80 h-80 relative bg-white rounded-full">
+                <img className="animate-shrink-grow absolute w-[66px] top-13 left-14" src="/miranda-mussas/SplitLogo/FlowerLogo.png" alt="Miranda Mussa Logo" />
+                <img className="animate-shrink-grow absolute w-[148px] right-13 top-13" src="/miranda-mussas/SplitLogo/TopRightLogo.png" alt="Miranda Mussa Logo" />
+                <img className="animate-shrink-grow absolute w-[208px] left-14 top-17" src="/miranda-mussas/SplitLogo/CenterLogo.png" alt="Miranda Mussa Logo" />
+                <img className="animate-shrink-grow absolute w-[141px] top-[116px] left-[54px]" src="/miranda-mussas/SplitLogo/BottomLeftLogo.png" alt="Miranda Mussa Logo" />
+                <img className="animate-shrink-grow absolute w-[99px] top-40 left-43" src="/miranda-mussas/SplitLogo/BottomRightLogo.png" alt="Miranda Mussa Logo" />
+              </div>
+            </div>)}
+
+            {/* absolute w-20
+            absolute w-50 top-5 right-6
+            absolute w-60 left-14 top-15
+            */}
+
+
     <header className="flex flex-col justify-between items-center bg-linear-75 from-rose-300 from-40% via-pink-200 via-60% to-rose-300 to-80% text-black">
       <div className="w-full flex justify-center">
         <img className="h-30 my-3" src="/miranda-mussas/mirandaMussaLogo.png" alt="Miranda Mussa Logo" />
